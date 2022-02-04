21899306793925e954491582ed8fdafb56428af0
demoXieSL <98636393+demoXieSL@users.noreply.github.com>
2022-02-04 19:24:44 +0800

    pull test 2

--- src/main/java/com/git/testGit.java
+++ src/main/java/com/git/testGit.java
@@ -3,7 +3,7 @@ package com.git;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("test git!");
-        System.out.println("test git2!");
+        System.out.println("test git2!冲突");
         System.out.println("GitHub pull --1");
         System.out.println("test git3!");
         System.out.println("master---test");
