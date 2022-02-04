7b3cc74f41e74ec364b522bd3e0e65c121605988
demoXieSL <98636393+demoXieSL@users.noreply.github.com>
2022-02-04 19:12:40 +0800

    Update testGit.java

--- src/main/java/com/git/testGit.java
+++ src/main/java/com/git/testGit.java
@@ -4,9 +4,11 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("test git!");
         System.out.println("test git2!");
+        System.out.println("GitHub pull --1");
         System.out.println("test git3!");
         System.out.println("master---test");
         System.out.println("dev---test");
         System.out.println("GitHub push test");
+        System.out.println("GitHub pull --2");
     }
 }
